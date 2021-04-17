2aa664f37e26e7c054084f13c894be9719500137
Dallin Urness <dallin.urness96@gmail.com>
2021-04-17 11:49:02 -0600

    just delete instead of edit for rate limit

--- clock_bot.py
+++ clock_bot.py
@@ -3,9 +3,9 @@ from discord.ext import commands, tasks
 from datetime import datetime
 import pytz
 
-#import logging
+import logging
 
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 
 from dotenv import load_dotenv
@@ -16,38 +16,61 @@ load_dotenv()
 # Client
 client = commands.Bot(command_prefix='clock--', help_command=None)
 
-timezone = None
-saved_guild = None
-twelve_hour = False
+saved_guilds = []
 
-@tasks.loop(minutes=6)
-async def update_time():
-    if timezone == None or saved_guild == None:
-        return
-    
+def get_time(timezone, twelve_hour):
     time_string = str(datetime.now(timezone).strftime('%H:%M'))
     
     if int(time_string.split(":")[0]) == 0:
         time_string = "12:" + time_string.split(":")[1]
 
+    is_pm = False
+    if int(time_string.split(":")[0]) == 12:
+        is_pm = True
+
     # handle twelve hour
     if twelve_hour and int(time_string.split(":")[0]) > 12:
+        is_pm = True
         time_string = str(int(time_string.split(":")[0]) - 12) + ":" + time_string.split(":")[1]
 
-    new_title = 'servertime ' + time_string
+    am_pm = "pm" if is_pm else "am"
+    time_string = 'servertime ' + time_string + am_pm
+    return time_string
+
+@tasks.loop(minutes=1)
+async def update_time():
+    global saved_guilds
 
     #update previous channel
+    for saved_guild in saved_guilds:
+        new_title = get_time(saved_guild["timezone"], saved_guild["twelve_hour"])
+        channel_exists = False
+        for channel in saved_guild["guild"].channels:
+            first_word = channel.name.split(" ")[0]
+            if first_word == 'servertime':
+                await channel.delete()
+                #channel_exists = True
+                #await channel.edit(name=new_title)
+                #break
+
+        #create new channel
+        if not channel_exists:
+            await saved_guild["guild"].create_voice_channel(new_title)
+
+async def initialize_timekeeper(context, timezone, twelve_hour):
+    new_title = get_time(timezone, twelve_hour)
     channel_exists = False
-    for channel in saved_guild.channels:
+    for channel in context.guild.channels:
         first_word = channel.name.split(" ")[0]
         if first_word == 'servertime':
-            channel_exists = True
-            await channel.edit(name=new_title)
-            break
+            await channel.delete()
+            #channel_exists = True
+            #await channel.edit(name=new_title)
+            #break
 
     #create new channel
     if not channel_exists:
-        await saved_guild.create_voice_channel(new_title)
+        await context.guild.create_voice_channel(new_title)
 
 @update_time.before_loop
 async def before_update_time():
@@ -55,14 +78,12 @@ async def before_update_time():
 
 @update_time.after_loop
 async def after_update_time():
-    print('loop has stopped')
+    print("loop unexpectedly ended")
 
 @client.command(name='set_timezone')
 async def set_timezone(context, *args):
-    global saved_guild
-    global timezone
-    global twelve_hour
-    global update_time
+    global saved_guilds
+
     try:
         timezone = pytz.timezone(args[0])
     except:
@@ -73,32 +94,25 @@ async def set_timezone(context, *args):
         if args[1] == "twelve_hour":
             twelve_hour = True
 
-    if saved_guild != None:
-        return
+    #check if we are just changing the timezone of this guild
+    for saved_guild in saved_guilds:
+        if context.guild.id == saved_guild["guild"].id:
+            saved_guild["timezone"] = timezone
+            saved_guild["twelve_hour"] = twelve_hour
+            return
 
-    saved_guild = context.guild
+    saved_guilds.append({"guild": context.guild, "timezone": timezone, "twelve_hour": twelve_hour})
 
-    update_time.start()
+    await initialize_timekeeper(context, timezone, twelve_hour)
 
 @client.command(name='stop')
 async def stop(context):
-    global timezone
-    global twelve_hour
-    global saved_guild
-    global update_time
-    timezone = None
-    twelve_hour = None
-    
-    update_time.cancel()
-
-    if saved_guild != None:
-        for channel in saved_guild.channels:
-            first_word = channel.name.split(" ")[0]
-            if first_word == 'servertime':
-                channel_exists = True
-                await channel.delete()
-                break
-        saved_guild = None
+    # remove channel
+    for channel in context.guild.channels:
+        first_word = channel.name.split(" ")[0]
+        if first_word == 'servertime':
+            await channel.delete()
+            break
 
     await context.send("Server Clock has been deactivated")
 
@@ -115,4 +129,5 @@ async def help_funct(context):
     await context.send(embed=em)
 
 # run the client on server
+update_time.start()
 client.run(os.getenv("SECRET_CODE"))
